de1c12d0160ece3b052e000e9d7890ae146ba7ce
Ben Sonley <lbsonley@gmail.com>
2021-04-30 10:11:25 +0200

    chore(sidebar-layout): define height for main-content; re-order navigation items

--- src/components/layouts/sidebar-layout.js
+++ src/components/layouts/sidebar-layout.js
@@ -10,6 +10,11 @@ const SidebarLayout = ({ children }) => {
         </div>
         <nav className="layout-sidebar__navigation">
           <ul className="layout-sidebar__navigation-list">
+            <li className="layout-sidebar__navigation-list-item">
+              <p className="layout-sidebar__nagivation-list-link">
+                Home
+              </p>
+            </li>
             <li className="layout-sidebar__navigation-list-item layout-sidebar__navigation-list-item--active">
               <p className="layout-sidebar__nagivation-list-link">
                 Daily Nutrition
@@ -18,11 +23,6 @@ const SidebarLayout = ({ children }) => {
             <li className="layout-sidebar__navigation-list-item">
               <p className="layout-sidebar__nagivation-list-link">
                 Nuritional Trends
-              </p>
-            </li>
-            <li className="layout-sidebar__navigation-list-item">
-              <p className="layout-sidebar__nagivation-list-link">
-                Nuritional Targets
               </p>
             </li>
             <li className="layout-sidebar__navigation-list-item">
